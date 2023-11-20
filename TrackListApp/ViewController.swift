b1bcf7a34e9fcde52ee14d9e1479cd13395b1852
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-11-20 11:16:06 +0700

    add cell, embed in navigation controller

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+         
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //метод нужен для определения индекса (пути к) ячейки с какими то данными.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell index \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
