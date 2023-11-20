cead4530379510c7837a84d09d62800da1384801
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-11-20 15:43:58 +0700

    add album image, setup it, add second vc

---
+++ TrackListApp/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  TrackListApp
+//
+//  Created by D i on 20.11.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    var trackName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -36,62 +36,40 @@ class TrackListViewController: UITableViewController {
         if #available(iOS 14, *) {
             var content = cell.defaultContentConfiguration()
             content.text = trackList[indexPath.row]
-            content.image = UIImage(systemName: "plus.circle.fill", variableValue: 0, configuration: UIImage.SymbolConfiguration(weight: .light))?.withTintColor(.systemTeal, renderingMode: .alwaysOriginal)
+            content.textProperties.numberOfLines = 0
+            
+            content.image = UIImage(named: trackList[indexPath.row])
+            content.imageProperties.maximumSize = CGSize(width: 43, height: 43)
             cell.contentConfiguration = content
             
         } else {
             cell.textLabel?.text = trackList[indexPath.row]
-            cell.imageView?.image = UIImage(systemName: "plus.circle.fill", variableValue: 0, configuration: UIImage.SymbolConfiguration(weight: .light))?.withTintColor(.systemTeal, renderingMode: .alwaysOriginal)
+            cell.textLabel?.numberOfLines = 0
+            cell.imageView?.image = UIImage(named: trackList[indexPath.row])
+            cell.imageView?.translatesAutoresizingMaskIntoConstraints = false
+            
+            cell.imageView?.clipsToBounds = true
+            cell.imageView?.layer.masksToBounds = true
+            cell.imageView?.contentMode = .scaleAspectFit
 
         }
     
         return cell
     }
     
+    //MARK: - Table View Delegate
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 60
+//    }
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//  
 
 }
